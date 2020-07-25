9a178293ea2248883391036881ca5b38b48afe62
sommer <sommer1111@163.com>
2020-07-25 18:43:02 +0800

    更新二叉树

--- 数据结构与算法学习/Tree/BinaryTreeOrderDemo.java
+++ 数据结构与算法学习/Tree/BinaryTreeOrderDemo.java
@@ -2,15 +2,25 @@ package DataStructureTest.Tree;
 
 
 /**
- * 建立二叉树，（参考哈希表）分析实现：
+ * 一、建立二叉树，（参考哈希表）分析实现：
  * 1. 建立节点类 TreeNode类，属性：自身、左子树、右子树、各种方法
  * 2. 建立树类 TreeLink，大树撑起一片天，具体实现整棵树的前序遍历、中序遍历、后序遍历
  *  |---根节点这里有些疑惑
  *  |---对 return 什么有疑惑，并且对于 Treenode 里面的方法，
  *      和 TreeLink 里面方法的关系不知如何组织。
  *
- *  二叉树的前序、中序、后序遍历
- *  二叉树的前序、中序、后序查找
+ * 二、前序、中序、后序遍历的区别：父节点输出的顺序。
+ * 前序遍历：先输出父节点-左子节点-右子节点
+ * 中序遍历：先输出左子节点-父节点-右子节点
+ * 后序遍历：先输出左子节点-右子节点-父节点
+ *
+ * 三、具体实现方法
+ * 1.首先需要节点类：TreeNode，它本身提供遍历查找删除的功能
+ * 2.其次需要将整个节点组织起来的二叉树类，TreeLink类，提供实际可以调用的遍历查找删除的方法。
+ * 而实际其内部实现也只是调用节点本身的各种方法。
+ *    |--- 二叉树的前序、中序、后序遍历
+ *    |--- 二叉树的前序、中序、后序查找
+ *    |--- 删除节点
  *
  * @Auther:sommer1111
  * @date 2020/7/15 14:33
@@ -31,25 +41,31 @@ public class BinaryTreeOrderDemo {
         node2.setLeft(node4);
         node3.setRight(node5);
 
-        System.out.println("前序遍历：");
+//        System.out.println("前序遍历：");
+//        tree.preView();
+//        System.out.println();
+//        System.out.println("中序遍历：");
+//        tree.midView();
+//        System.out.println();
+//        System.out.println("后序遍历：");
+//        tree.postView();
+//
+//        System.out.println("-----------");
+//        System.out.println("前序查找：");
+//        tree.preFind(node5);
+//        System.out.println();
+//        System.out.println("中序查找：");
+//        tree.midFind(node5);
+//        System.out.println();
+//        System.out.println("后序查找：");
+//        tree.postFind(node5);
+
+        System.out.println("删除前，前序遍历：");
         tree.preView();
         System.out.println();
-        System.out.println("中序遍历：");
-        tree.midView();
-        System.out.println();
-        System.out.println("后序遍历：");
-        tree.postView();
-
-        System.out.println("-----------");
-        System.out.println("前序查找：");
-        tree.preFind(node5);
-        System.out.println();
-        System.out.println("中序查找：");
-        tree.midFind(node5);
-        System.out.println();
-        System.out.println("后序查找：");
-        tree.postFind(node5);
-
+        System.out.println("删除后前序遍历：");
+        tree.delNode(3);
+        tree.preView();
 
 
     }
@@ -188,6 +204,38 @@ class TreeNode{
     }
 
 
+    //删除
+    //1.如果为叶子节点，则直接删除。
+    //2.如果非叶子节点，则把以下的所有部分都删除。
+    //分析步骤
+    //1.判断是否为根节点，是则直接置为空。
+    //2.判断左子节点，是则删除
+    //3.如果左子节点不等，判断右子节点。
+    //依旧没有找到，则递归左子节点、右子节点找
+
+    public void delNode(int no){
+        if(this.getLeft() != null && this.getLeft().getId() == no){
+            this.setLeft(null);
+            return;
+        }
+
+        if(this.getRight() != null && this.getRight().getId()== no){
+            this.setRight(null);
+            return;
+        }
+
+        //都找不到，则递归查找删除
+        if(this.getLeft()!=null){
+            this.getLeft().delNode(no);
+        }
+
+        if(this.getRight()!=null){
+            this.getRight().delNode(no);
+        }
+    }
+
+
+
 }
 
 class TreeLink{
@@ -248,6 +296,17 @@ class TreeLink{
             System.out.println("二叉树为空，无法遍历！");
         }
     }
-}
 
+    public void delNode(int no){
+        if(this.root != null){
+            if(this.root.getId()==no){
+                this.root = null;
+            }else{
+                this.root.delNode(no);
+            }
+        }else{
+            System.out.println("该节点不存在！");
+        }
+    }
+}
 
